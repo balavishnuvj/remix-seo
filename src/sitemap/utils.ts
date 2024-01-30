b7bb1924ac3dbea3d7433ad5a2a198d94f16d7fb
Leo Singer <leo.singer@ligo.org>
2024-01-30 17:31:17 -0500

    Revert "Replace lodash dependency with lodash.isequal. (#9)"
    This reverts commit fe0bc539aff86792456bd3bcd9993f92ab232849.

--- src/sitemap/utils.ts
+++ src/sitemap/utils.ts
@@ -1,7 +1,7 @@
 // This is adapted from https://github.com/kentcdodds/kentcdodds.com
 
 import { EntryContext } from "@remix-run/server-runtime";
-import isEqual from "lodash.isequal";
+import { isEqual } from "lodash";
 import { SEOHandle, SitemapEntry } from "../types";
 
 type Options = {
